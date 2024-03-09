cddb20787eeae8d910f35da71b6746b61b34fbe5
John Swaynos <jpswaynos@gmail.com>
2024-03-09 12:49:40 -0800

    Added another script to ingest files for querying.

---
+++ librarian.py
@@ -0,0 +1,34 @@
+from llama_index.core import SimpleDirectoryReader
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import VectorStoreIndex
+from llama_index.llms.ollama import Ollama
+
+embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
+loader = SimpleDirectoryReader(
+    input_dir="./data/",
+    recursive=True,
+    required_exts=[".epub"],
+)
+
+documents = loader.load_data()
+
+index = VectorStoreIndex.from_documents(
+    documents,
+    embed_model=embedding_model,
+)
+
+llama = Ollama(
+    model="llama2",
+    request_timeout=300.0,
+)
+
+query_engine = index.as_query_engine(llm=llama)
+
+print("Query Engine is Running.\n")
+
+print(
+    query_engine.query(
+        "What are the titles of all the books available? Show me the context used to derive your answer."
+    )
+)
