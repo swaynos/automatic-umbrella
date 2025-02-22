2482a9a878160918148c6b1365ee45c3f85860e6
John Swaynos <jpswaynos@gmail.com>
2025-02-21 21:35:06 -0800

    Overhauled the README, and changed the configuration options.

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # automatic-umbrella
-Repurposing this repo to instead be used for some web automation.
+Web automation for EA FC25.
 
-## my_secrets.py
-This project will require you to provide your email and password for authentication. Please use my_secrets.py as a template to create your own secrets.py file with the following 2 variables:
+## .env
+This file will be used to store sensitive information such as email and password, as well as user configuration settings. Create this file, and here are the minimum expected variables:
+```
 EMAIL = "your_email@email.com"
 PASSWORD = "your_password"
-
+DAILY_SIMPLE_BRONZE_SBC_NAMES=Daily Bronze Upgrade
+DAILY_SIMPLE_SILVER_SBC_NAMES=Daily Silver Upgrade
+PACK_NAMES=BRONZE PLAYERS PREMIUM,SMALL BRONZE PLAYERS,SILVER PLAYERS PREMIUM,Small Silver Players Pack,Super Bronze Pack
+GOLD_PACK_NAMES=x11 Gold Players Pack
+```
 <br>
-TODO: Move secrets to a .env file
+
+These variables can be used to toggle the flow control of the application:
+```
+OPEN_GOLD_PACKS=True
+OPEN_CHEAP_PACKS=True
+SOLVE_DAILY_CHALLENGES=True
+GRASSROOT_GRIND=True
+TOTY_CRAFTING_UPGRADE=True
+EIGHTYONE_PLUS_PLAYER_PICK=True
+```
+
+Please see ```config.py``` for other configuration options.
 
 ## setup.py
 This script was written when moving code to a new machine. It will help resolve issues with webdriver-manager. On macOS there may be an additional step outside of the IDE to allow execution of the binaries through the operating system | Security settings.
--- src/config.py
+++ src/config.py
@@ -5,13 +5,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Global static config
-APP_URL = os.getenv("APP_URL")
+APP_URL = os.getenv("APP_URL", "https://www.ea.com/ea-sports-fc/ultimate-team/web-app/")
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 DEFAULT_WAIT_DURATION = int(os.getenv("DEFAULT_WAIT_DURATION", 10))
 LONGER_WAIT_DURATION = int(os.getenv("LONGER_WAIT_DURATION", DEFAULT_WAIT_DURATION + 15))
-COOKIES_FILE = os.getenv("COOKIES_FILE")
+COOKIES_FILE = os.getenv("COOKIES_FILE", "cookies.json")
 DAILY_SIMPLE_BRONZE_SBC_NAMES = os.getenv("DAILY_SIMPLE_BRONZE_SBC_NAMES", "Daily Bronze Upgrade").split(',')
 DAILY_SIMPLE_SILVER_SBC_NAMES = os.getenv("DAILY_SIMPLE_SILVER_SBC_NAMES", "Daily Silver Upgrade").split(',')
 PACK_NAMES = os.getenv("PACK_NAMES", "BRONZE PLAYERS PREMIUM,SMALL BRONZE PLAYERS,SILVER PLAYERS PREMIUM,Small Silver Players Pack,Super Bronze Pack").split(',')
 GOLD_PACK_NAMES = os.getenv("GOLD_PACK_NAMES", "x11 Gold Players Pack").split(',')
+
+OPEN_GOLD_PACKS = os.getenv("OPEN_GOLD_PACKS", "false").lower() in ("true", "1", "t")
+OPEN_CHEAP_PACKS = os.getenv("OPEN_CHEAP_PACKS", "false").lower() in ("true", "1", "t")
+SOLVE_DAILY_CHALLENGES = os.getenv("SOLVE_DAILY_CHALLENGES", "true").lower() in ("true", "1", "t")
+GRASSROOT_GRIND = os.getenv("GRASSROOT_GRIND", "false").lower() in ("true", "1", "t")
+TOTY_CRAFTING_UPGRADE = os.getenv("TOTY_CRAFTING_UPGRADE", "false").lower() in ("true", "1", "t")
+EIGHTYONE_PLUS_PLAYER_PICK = os.getenv("EIGHTYONE_PLUS_PLAYER_PICK", "false").lower() in ("true", "1", "t")
